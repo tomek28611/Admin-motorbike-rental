60ae6e93148b645b050e9299176816760ae0b224
Tomek28611 <tomek286@yahoo.com>
2023-05-14 10:04:35 +0200

    secret2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,6 @@ export default NextAuth({
       clientId: "977689590500-ssep6bulk7ju3tadu6p2kuglj596hcid.apps.googleusercontent.com",
       clientSecret: "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"
     }),
-  ]
+  ],
+  secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
 })
