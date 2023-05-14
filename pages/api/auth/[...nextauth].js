488b993825edb8792624a0ac35e3828bb7d9a5e8
Tomek28611 <tomek286@yahoo.com>
2023-05-14 09:53:31 +0200

    secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,12 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from'next-auth/providers/google';
 
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientId: "977689590500-ssep6bulk7ju3tadu6p2kuglj596hcid.apps.googleusercontent.com",
+      clientSecret: "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"
     }),
   ]
 })
