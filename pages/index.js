9650d17b8e23c66937a736db684ebd90664af624
Tomek28611 <tomek286@yahoo.com>
2023-05-14 12:55:21 +0200

    session

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import {useSession} from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) return;
 
   return <Layout>
+    <div className="">{session?.user?.name}</div>
     <div className="">{session?.user?.email}</div>
+
   </Layout>;
 
 }
