30d81f73bf478c968bc1a75a5eb7689843063407
Tomek28611 <tomek286@yahoo.com>
2023-05-16 13:15:08 +0200

    images to aws

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -10,7 +10,8 @@ export default function ProductForm({
     pricePerDay: existingPricePerDay,
     pricePerWeek: existingPricePerWeek,
     pricePerMonth: existingPricePerMonth,
-    description: existingDescription
+    description: existingDescription,
+    images
 }) {
     const [name, setName] = useState(existingName || '');
     const [deposit, setDeposit] = useState(existingDeposit || '');
@@ -34,7 +35,7 @@ export default function ProductForm({
         }
         if (_id) {
             //update
-            await axios.put('/api/products',{...data,_id});
+            await axios.put('/api/products', { ...data, _id });
         } else {
             //create
             await axios.post('/api/products', data);
@@ -43,6 +44,23 @@ export default function ProductForm({
     }
     if (goToProducts) {
         router.push('/products');
+    }
+
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+           
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file)
+            }
+            const res = await fetch('/api/upload',  {
+                method: 'POST',
+                body: data,
+            });
+            console.log(res);
+ 
+        }
     }
 
     return (
@@ -54,6 +72,22 @@ export default function ProductForm({
                 placeholder="motorbike brand and model"
                 value={name}
                 onChange={ev => setName(ev.target.value)} />
+
+            <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-24 h-24 border text-center rounded-lg flex flex-col items-center justify-center text-sm text-gray-500 bg-orange-200 cursor-pointer hover:bg-gray-100 hover:text-black hover:font-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type="file" onChange={uploadImages} className="hidden" />
+                </label>
+                {!images?.lenght && (
+                    <div>No photos</div>
+                )}
+            </div>
 
             <label>Deposit</label>
             <input
